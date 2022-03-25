88af446978cbedcf7c278241a1a56dddf34b119a
Matthew Clarke <m_d_clarke@hotmail.com>
2022-03-25 14:06:09 +0200

    added L5 - Prefix Sums - PassingCars

---
+++ L-5_Prefix-Sums_PassingCars.js
@@ -0,0 +1,20 @@
+// Count the number of passing cars on the road.
+// Problem description: https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+// Detected complexity: O(N)
+
+function solution(A) {
+    const ALen = A.length;
+    let pairCount = 0;
+    let lastEastIndex = 0;
+
+    for (let i = 0; i < ALen; i += 1) {
+        if (A[i] === 0) {
+            lastEastIndex += 1;
+        } else {
+            // for each lastEastIndex - there is an additional pair
+            pairCount += lastEastIndex;
+        }
+    }
+
+    return (pairCount > 1000000000) ? -1 : pairCount;
+}
